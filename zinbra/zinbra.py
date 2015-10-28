f72915644af8638ee517bf6360a27c62e9e7bea4
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2015-10-28 20:05:34 +0300

    Support comparison action

--- zinbra/zinbra.py
+++ zinbra/zinbra.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
 Python wrapper for zinbra.xml
-Usage: zinbra.py "${genome}" "${bed}" "${bin}" "${fdr}"
 """
 
 import os
@@ -10,7 +9,14 @@ import subprocess
 
 argv = sys.argv[1:]
 print 'Arguments {0}'.format(argv)
-genome, bed, bin, fdr = argv
+
+# Check command
+if len(argv) == 5:
+    genome, bin, fdr, action, bed = argv
+    assert action == "analyze"
+else:
+    genome, bin, fdr, action, bed1, bed2 = argv
+    assert action == "compare"
 
 # Configure main jar path
 jar = os.environ.get("INTEGRATION_JAR")
@@ -21,10 +27,11 @@ print 'Converting reference genome fasta to 2bit: {0}'.format(cmd)
 subprocess.check_call(cmd, cwd=None, shell=True)
 
 # See https://github.com/JetBrains-Research/zinbra for command line options
-# cla.argument_string_list() is configured at ZinbraApplications#rebuildArgumentStringsFromVisualOptions
-cmd = 'java -cp {0} org.jetbrains.bio.zinbra.ZinbraCLA ' \
-      'analyze -i {1} -bed result.bed -r reference.2bit -b {3} -fdr {4}'.format(jar,
-                                                                                bed,
-                                                                                bin, fdr)
+if action == "analyze":
+    cmd = 'java -cp {0} org.jetbrains.bio.zinbra.ZinbraCLA ' \
+          'analyze -i {1} -bed result.bed -r reference.2bit -b {3} -fdr {4}'.format(jar, bed, bin, fdr)
+else:
+    cmd = 'java -cp {0} org.jetbrains.bio.zinbra.ZinbraCLA ' \
+          'compare -1 {1} -2 {2} -bed result.bed -r reference.2bit -b {3} -fdr {4}'.format(jar, bed1, bed2, bin, fdr)
 print 'Launching zinbra: {0}'.format(cmd)
 subprocess.check_call(cmd, cwd=None, shell=True)
