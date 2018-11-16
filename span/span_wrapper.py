8d6866a0e74a1d4f1d086b6a188c22d2a51b565c
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2018-11-16 19:44:26 +0300

    Successfully launch SPAN without presenting correct output

--- span/span_wrapper.py
+++ span/span_wrapper.py
@@ -1,44 +1,79 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import subprocess
+import sys
+
 argv = sys.argv[1:]
 print 'Arguments {0}'.format(argv)
 
 SPAN_JAR = os.environ.get("SPAN_JAR")
-# span.jar from Docker container
-# SPAN_JAR = "/root/span.jar"
 print 'Using SPAN Peak Analyzer distributive file {0}'.format(SPAN_JAR)
 
-# #if $action.action_selector
-#     #if str($control.control_selector) == "with_control"
-#         span_wrapper.py model with_control "${genome}" "${treatment_file}" "${bin}" "${action.model_file}" "${control.control_file}"
+# #if str($action.action_selector) == "model"
+#     #if $control.control_selector
+#         span_wrapper.py model with_control
+#             "${genome_identifier}" "${genome_file}"
+#             "${treatment_identifier}" "${treatment_file}"
+#             "${bin}" "${action.model_file}"
+#             "${control_identifier}" "${control.control_file}"
 #     #else
-#         span_wrapper.py model without_control "${genome}" "${treatment_file}" "${bin}" "${action.model_file}"
+#         span_wrapper.py model without_control
+#             "${genome_identifier}" "${genome_file}"
+#             "${treatment_identifier}" "${treatment_file}"
+#             "${bin}" "${action.model_file}"
 #     #end if
 # #else
 #     #if $control.control_selector
-#         span_wrapper.py peaks with_control "${genome}" "${treatment_file}" "${bin}" "${action.model_file}" "${control.control_file}" "${fdr}" "${gap}" "${action.peaks_file}"
+#         span_wrapper.py peaks with_control
+#             "${genome_identifier}" "${genome_file}"
+#             "${treatment_identifier}" "${treatment_file}"
+#             "${bin}" "${action.model_file}"
+#             "${control_identifier}" "${control.control_file}"
+#             "${fdr}" "${gap}" "${action.peaks_file}"
 #     #else
-#         span_wrapper.py peaks without_control "${genome}" "${treatment_file}" "${bin}" "${action.model_file}" "${fdr}" "${gap}" "${action.peaks_file}"
+#         span_wrapper.py peaks with_control
+#             "${genome_identifier}" "${genome_file}"
+#             "${treatment_identifier}" "${treatment_file}"
+#             "${bin}" "${action.model_file}"
+#             "${fdr}" "${gap}" "${action.peaks_file}"
 #     #end if
 # #end if
 
 # See http://artyomovlab.wustl.edu/aging/span.html for command line options
 action = argv[0]
 control = argv[1]
+
+
+def link(name, f):
+    result = os.path.join(os.path.abspath('.'), name)
+    os.symlink(f, result)
+    return result
+
+
 if action == 'model':
     if control == 'with_control':
-        (chrom_sizes, treatment_file, bin, model_file, control_file) = argv[2:]
+        (chrom_sizes, chrom_sizes_file,
+         treatment, treatment_file,
+         bin, model_file,
+         control, control_file) = argv[2:]
         cmd = 'java -jar {} analyze --chrom.sizes {} --treatment {} --control {} --bin {}'.format(
-            SPAN_JAR, chrom_sizes, treatment_file, control_file, bin
+            SPAN_JAR,
+            link(chrom_sizes, chrom_sizes_file),
+            link(treatment, treatment_file),
+            link(control, control_file),
+            bin
         )
         print "MODEL FILE" + model_file
     elif control == 'without_control':
-        (chrom_sizes, treatment_file, bin, model_file) = argv[2:]
+        (chrom_sizes, chrom_sizes_file,
+         treatment, treatment_file,
+         bin, model_file) = argv[2:]
         cmd = 'java -jar {} analyze --chrom.sizes {} --treatment {} --bin {}'.format(
-            SPAN_JAR, argv[2], argv[3], argv[4]
+            SPAN_JAR,
+            link(chrom_sizes, chrom_sizes_file),
+            link(treatment, treatment_file),
+            bin
         )
         print "MODEL FILE" + model_file
     else:
@@ -46,22 +81,37 @@ if action == 'model':
 
 elif action == "peaks":
     if control == 'with_control':
-        (chrom_sizes, treatment_file, bin, model_file, control_file, fdr, gap, peaks_file) = argv[2:]
+        (chrom_sizes, chrom_sizes_file,
+         treatment, treatment_file,
+         bin, model_file,
+         control, control_file,
+         fdr, gap, peaks_file) = argv[2:]
         cmd = 'java -jar {} analyze --chrom.sizes {} --treatment {} --control {} --bin {} --fdr {} --gap {} --peaks {}'.format(
-            SPAN_JAR, chrom_sizes, treatment_file, control_file, bin, fdr, gap, peaks_file
+            SPAN_JAR,
+            link(chrom_sizes, chrom_sizes_file),
+            link(treatment, treatment_file),
+            link(control, control_file),
+            bin, fdr, gap,
+            peaks_file
         )
         print "MODEL FILE" + model_file
     elif control == 'without_control':
-        (chrom_sizes, treatment_file, bin, model_file, fdr, gap, peaks_file) = argv[2:]
+        (chrom_sizes, chrom_sizes_file,
+         treatment, treatment_file,
+         bin, model_file,
+         fdr, gap, peaks_file) = argv[2:]
         cmd = 'java -jar {} analyze --chrom.sizes {} --treatment {} --bin {} --fdr {} --gap {} --peaks {}'.format(
-            SPAN_JAR, chrom_sizes, treatment_file, bin, fdr, gap, peaks_file
+            SPAN_JAR,
+            link(chrom_sizes, chrom_sizes_file),
+            link(treatment, treatment_file),
+            bin, fdr, gap,
+            peaks_file
         )
         print "MODEL FILE" + model_file
     else:
         raise Exception("Unknown control option {}".format(control))
 else:
     raise Exception("Unknown action command {}".format(action))
-
 
 print 'Launching SPAN: {0}'.format(cmd)
 subprocess.check_call(cmd, cwd=None, shell=True)
